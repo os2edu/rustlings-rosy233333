d588f6e876673dcb9785b2642132751c0acbfac9
Rosy <lc_rosy@qq.com>
2022-11-08 15:38:10 +0800

    finished:     functions     if     quiz1     primitive_types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,11 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+ fn calculate_price_of_apples(a:u32)->u32 {
+    if a <= 40{
+        a * 2
+    } else {
+        a
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
